642779b56894d6f79886cc28fb067ad9ba898c32
Liana <75253715+SupernaturalOrange@users.noreply.github.com>
2023-07-02 18:16:32 +0300

    feat/clickhouse (#11)
    Co-authored-by: liana <liana.nigmatullina@tetrika.school>

---
+++ clickhouse/data/node1/init_node_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE shard;
+CREATE DATABASE replica;
+CREATE TABLE shard.movie_views (movie_id UUID, user_id UUID, view_progress UInt16, created_at timestamp) Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/movie_views', 'replica_1') PARTITION BY toYYYYMMDD(created_at) ORDER BY (user_id, movie_id);
+CREATE TABLE replica.movie_views (movie_id UUID, user_id UUID, view_progress UInt16, created_at timestamp) Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/movie_views', 'replica_2') PARTITION BY toYYYYMMDD(created_at) ORDER BY (user_id, movie_id);
+CREATE TABLE default.movie_views (movie_id UUID, user_id UUID, view_progress UInt16, created_at timestamp) ENGINE = Distributed('company_cluster', '', movie_views, rand());
