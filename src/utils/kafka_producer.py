32189db8c8dc2d03c3da4f2db4256169af0fb112
tavrora <46925576+tavrora@users.noreply.github.com>
2023-07-04 14:07:44 +0300

    Update src/utils/kafka_producer.py
    Co-authored-by: Liana <75253715+SupernaturalOrange@users.noreply.github.com>

--- src/utils/kafka_producer.py
+++ src/utils/kafka_producer.py
@@ -10,7 +10,7 @@ class AbstractProducer(ABC):
     @abstractmethod
     def send(self, topic: str, value: bytes, key: bytes):
         """Метод отправки данных в хранилище"""
-        pass
+        raise NotImplementedError
 
 
 @dataclass
