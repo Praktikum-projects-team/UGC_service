8d756e5a43870ba8577c5419909b86317a06b277
Tatyana Akimova <takimova@ivi.ru>
2023-07-05 18:13:14 +0300

    refactoring mvc

--- src/api/v1/ugc.py
+++ src/api/v1/ugc.py
@@ -1,11 +1,7 @@
-import json
-from datetime import datetime
-
 from fastapi import APIRouter, Depends, Request
 
 from api.v1.auth.auth_bearer import BaseJWTBearer
 from api.v1.models.ugc import EventWatch, EventWatchResp
-from core.config import kafka_config
 from services.auth_api import AuthApi
 from services.ugc import EventService, get_event_service
 
@@ -28,18 +24,6 @@ async def event_handler(
     Отправляем временную метку unix timestamp, соответствующую текущему месту просмотра фильма пользователем
     """
     current_user = request.token_payload  # type: ignore
-
-    data_event = {
-        "user_id": str(current_user["id"]),
-        "film_id": str(body.film_id),
-        "watch_time": str(body.watch_time),
-        "created_at": str(datetime.utcnow())
-    }
-
-    await event_service.send_event(
-        topic=kafka_config.kafka_topic,
-        value=bytearray(json.dumps(data_event), 'utf-8'),
-        key=bytearray(current_user["id"] + "+" + str(body.film_id), 'utf-8'),
-    )
+    await event_service.send_event(current_user, body)
 
     return EventWatchResp(msg='Event watch time successfully sent')
--- src/services/ugc.py
+++ src/services/ugc.py
@@ -1,5 +1,10 @@
+import json
+from datetime import datetime
+
 from fastapi import Depends
 
+from api.v1.models.ugc import EventWatch
+from core.config import kafka_config
 from utils.kafka_producer import AbstractEventProducer, KafkaProducer, get_event_producer
 
 
@@ -10,13 +15,24 @@ class EventService:
     def __init__(self, kafka: AbstractEventProducer):
         self.kafka = kafka
 
-    async def send_event(self, topic: str, value: bytes, key: bytes):
+    async def send_event(self, current_user: dict, event_info: EventWatch):
         """
         Метод отправки события в Kafka
-        :param topic: топик в Kafka
-        :param value: тело сообщения
-        :param key: ключ сообщения
+        :param current_user: информация о пользователе
+        :param event_info: информация о событии
         """
+
+        event_data = {
+            "user_id": str(current_user["id"]),
+            "film_id": str(event_info.film_id),
+            "watch_time": str(event_info.watch_time),
+            "created_at": str(datetime.utcnow())
+        }
+
+        topic = kafka_config.kafka_topic
+        value = bytearray(json.dumps(event_data), 'utf-8')
+        key = bytearray(current_user["id"] + "+" + str(event_data["film_id"]), 'utf-8')
+
         await self.kafka.send(topic=topic, value=value, key=key)
 
 
