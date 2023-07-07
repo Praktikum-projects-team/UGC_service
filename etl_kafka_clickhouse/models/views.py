f0182299d3415ab5e8fc7d6c1cec227e1536f5a1
Sofia <77271885+rsof14@users.noreply.github.com>
2023-07-07 13:02:23 +0300

    refactored etl (#15)
    * refactored etl
    
    * Fix models

--- etl_kafka_clickhouse/models/views.py
+++ etl_kafka_clickhouse/models/views.py
@@ -6,8 +6,8 @@ from datetime import datetime
 class MovieViews:
     movie_id: str
     user_id: str
-    view_progress: str
-    created_at: str
+    view_progress: int
+    created_at: datetime
 
 
 @dataclass
@@ -18,9 +18,9 @@ class Views:
     created_at: str
 
     def transform_to_ch(self):
-        created_at = datetime.fromisoformat(self.created_at).strftime("%Y-%m-%d %H:%M:%S")
+        created_at = datetime.fromisoformat(self.created_at)
         return MovieViews(movie_id=self.film_id, user_id=self.user_id,
-                          view_progress=self.watch_time, created_at=created_at)
+                          view_progress=int(self.watch_time), created_at=created_at)
 
 
 TOPICS_TABLES = {'views': (Views, 'movie_views')}
